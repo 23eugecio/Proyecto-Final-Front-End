9b0b4c8f53c6d744d04d0ff159a37588fcd42828
23eugecio <eugeciotti@gmail.com>
2024-08-12 10:27:05 -0300

    cambios

--- src/Context/GlobalContext.jsx
+++ src/Context/GlobalContext.jsx
@@ -14,33 +14,6 @@ export const GlobalProvider = ({ children }) => {
 
     const navigate = useNavigate();
 
-    const handleChangeFormValue = (e) => {
-        const { name, value } = e.target
-    const FormContactos = ({formContactos}) => {
-        return (    
-            <div>
-                <form>
-                    <label htmlFor="name">Nombre</label>
-                    <input type="text" id="name" placeholder="Escriba su nombre" />     
-                </form>
-    
-                <form>
-                    <label htmlFor="numero">Numero</label>
-                    <input type="text" id="numero" placeholder="Escriba su numero" />     
-                </form>     
-    
-                <form>
-                    <label htmlFor="estado">Estado</label>  
-                    <select name="estado" id="estado">
-                        <option value="online">Online</option>
-                        <option value="offline">Offline</option>
-                    </select>
-                </form>
-                
-            </div>
-        )
-    }  
-    } 
     const handleCreateContact = (contacts) => {
         const newContact = {
             ...contacts,

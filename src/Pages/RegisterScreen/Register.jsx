b8d4083c85d9a416a17dc13f4615e9b1970d7d68
23eugecio <eugeciotti@gmail.com>
2024-12-06 07:18:11 -0300

    falla el register

--- src/Pages/RegisterScreen/Register.jsx
+++ src/Pages/RegisterScreen/Register.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { extractFormData } from '../../utils/extractFormData'
 import useForm from '../../Hooks/useForm'
 import { POST, getUnnauthenticatedHeaders } from '../../fetching/http.fetching'
-import validateForm from '../../utils/validateForm'
 import './Register.css'
 import ENVIROMENT from '../../enviroment'
 
